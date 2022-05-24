1356a52df30db0c0a92a205ccc35be1a0c6d1134
JayendraCitrusbug <jayendra.citrusbug@gmail.com>
2022-05-24 14:28:37 +0530

    done

--- chat/app/urls.py
+++ chat/app/urls.py
@@ -3,6 +3,8 @@ from app import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
-    path('<str:room>/', views.RoomView.as_view(), name='room'),
     path('check/', views.CheckView.as_view(), name='check'),
+    path('room/<str:room>/', views.RoomView.as_view(), name='room'),
+    path('send/', views.SendView.as_view(), name='send'),
+    path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
 ]
--- chat/app/views.py
+++ chat/app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect, render
 from django.views import View
-
-# Create your views here.
+from .models import Room, Message
+from django.contrib.auth.models import User
+from django.http import HttpResponse, JsonResponse
 
 
 class HomeView(View):
@@ -9,17 +10,38 @@ class HomeView(View):
         return render(request, 'home.html')
 
 
-class RoomView(View):
-    def get(self, request, room):
-        return render(request, 'room.html')
-
-
 class CheckView(View):
-    def get(self, request, room):
-        return redirect('room')
-
     def post(self, request):
         room_name = request.POST['room_name']
         username = request.POST['username']
 
-        print(room_name, username)
+        if Room.objects.filter(name=room_name).exists():
+            return redirect('/room/'+room_name+'/?username='+username)
+        else:
+            return redirect('home')
+
+
+class RoomView(View):
+    def get(self, request, room):
+        username = request.GET.get('username')
+        room_details = Room.objects.get(name=room)
+        return render(request, 'room.html', {'username': username, 'room': room, 'room_details': room_details})
+
+
+class SendView(View):
+    def post(self, request):
+        message = request.POST['message']
+        username = request.POST['username']
+        room_id = request.POST['room_id']
+
+        new_message = Message.objects.create(
+            value=message, user=username, room=room_id)
+        new_message.save()
+        return HttpResponse(f'{message} sent successfully...!')
+
+
+def getMessages(request, room):
+    room_details = Room.objects.get(name=room)
+
+    messages = Message.objects.filter(room=room_details.id)
+    return JsonResponse({"messages": list(messages.values())})
